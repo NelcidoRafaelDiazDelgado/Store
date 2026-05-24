f0ad23477a58a6c98b125bcbc1e7a3670e57b49b
SGtrinkets <gomezstanley754@gmail.com>
2026-05-23 20:07:29 -0400

    discovery cambiado a false en omniauth.rb, enlaces cambiados para transicion suave entre localhost:3000 y el enlace de login de keycloak (localhost:8080) y vice versa

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -10,15 +10,16 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     name: :keycloak,
     scope: [ :openid, :email, :profile ],
     response_type: :code,
-    issuer: "http://keycloak:8080/realms/rails-dev",
-    discovery: true,
+    issuer: "http://localhost:8080/realms/rails-dev",
+    discovery: false,
     client_options: {
       identifier: ENV["KEYCLOAK_CLIENT_ID"],
       secret: ENV["KEYCLOAK_CLIENT_SECRET"],
       redirect_uri: ENV["KEYCLOAK_REDIRECT_URI"],
       # Explicitly set endpoints to work in Docker
       authorization_endpoint: "http://localhost:8080/realms/rails-dev/protocol/openid-connect/auth",
-      token_endpoint: "http://localhost:8080/realms/rails-dev/protocol/openid-connect/token",
-      userinfo_endpoint: "http://localhost:8080/realms/rails-dev/protocol/openid-connect/userinfo"
+      token_endpoint: "http://keycloak:8080/realms/rails-dev/protocol/openid-connect/token",
+      userinfo_endpoint: "http://keycloak:8080/realms/rails-dev/protocol/openid-connect/userinfo",
+      jwks_uri: "http://keycloak:8080/realms/rails-dev/protocol/openid-connect/certs"
     }
 end
